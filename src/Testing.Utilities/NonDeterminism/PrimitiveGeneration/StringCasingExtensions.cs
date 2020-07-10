910d1b4c2863af57ca4b970d1e72899a7a1c0a87
pete-restall <pete@restall.net>
2020-07-10 10:20:01 +0100

    Added StringCasingExtensions.

---
+++ src/Testing.Utilities/NonDeterminism/PrimitiveGeneration/StringCasingExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace Lophtware.Testing.Utilities.NonDeterminism.PrimitiveGeneration
+{
+	[SuppressMessage("ReSharper", "UnusedMember.Global", Justification = CodeAnalysisJustification.PublishedApi)]
+	public static class StringCasingExtensions
+	{
+		public static string AnyCase(this string str)
+		{
+			return new string(str.Select(ch =>
+				BooleanGenerator.Either(
+					() => char.ToUpperInvariant(ch),
+					() => char.ToLowerInvariant(ch)))
+				.ToArray());
+		}
+	}
+}
